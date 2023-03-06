83cfbd77d4e35d970de1ddf3edca7e59accefe8e
germanlasserre <germanlasserre@gmail.com>
2023-03-06 05:12:21 -0300

    agrego if en accessAdmin.js de la carpeta middlewares

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,4 @@
 module.exports = {
     index : (req,res) => res.render('index'),
-    admin : (req, res) => res.render('admin')
+    admin : (req,res) => res.render('admin')
 }
--- src/middlewares/accessAdmin.js
+++ src/middlewares/accessAdmin.js
@@ -3,9 +3,23 @@ const admins = ['Greta','Ada','Tim','Vim']
 module.exports = (req,res,next) => {
 
     if(admins.includes(req.query.admin)){
+        next() 
+        return res.redirect('/')    
+    } else {
+        res.send("No tienes los privilegios para ingresar")
+    }
+  }
+
+
+
+/* const admins = ['Greta','Ada','Tim','Vim']
+
+module.exports = (req,res,next) => {
+
+    if(admins.includes(req.query.admin)){
         next()
     }
 
     return res.redirect('/')    
     
-}
+}  */
